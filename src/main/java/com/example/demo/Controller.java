db0ff8ac352016b1964a7f58072eec2a36736045
icemanek <emil_sankowski@tlen.pl>
2019-05-04 16:36:49 +0200

    test

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
 
 
     @GetMapping(value = "/")
