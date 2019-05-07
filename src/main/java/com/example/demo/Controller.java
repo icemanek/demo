85c3d2843729ae284ba99e153dda61a3bdd1da12
icemanek <emil_sankowski@tlen.pl>
2019-05-07 16:48:55 +0200

    webflux hello world

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,29 +1,30 @@
 package com.example.demo;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class Controller {
 
-    @Value("${param}")
-    private String startParam;
+//    @Value("${param}")
+//    private String startParam;
+//
+//    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Map<String, String> helloWorld() {
+//
+//        Map<String, String> hello = new HashMap<>();
+//
+//        hello.put("message", "Hello World");
+//        hello.put("startParam", startParam);
+//
+//
+//        return hello;
+//    }
 
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> helloWorld(){
-
-        Map<String, String> hello = new HashMap<>();
-
-        hello.put("message", "Hello World");
-        hello.put("startParam",startParam);
-        
-
-        return hello;
-    }
+        @GetMapping("/new")
+        public Mono<String> helloWorldNwe(){
+            return Mono.just("Nowy hello world!");
+        }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class DemoApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
