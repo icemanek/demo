0d1d90f2dcee068296d0a80f93de0f6cf19477ae
icemanek <emil_sankowski@tlen.pl>
2019-05-07 16:05:42 +0200

    przekazanie obiektu hello world w postaci Jsona

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,18 +1,29 @@
 package com.example.demo;
 
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
 
     @Value("${param}")
-    private String newParameter;
+    private String startParam;
 
-    @GetMapping(value = "/")
-    public String helloWorld(){
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> helloWorld(){
 
-        return "Hello world  " + newParameter ;
+        Map<String, String> hello = new HashMap<>();
+
+        hello.put("message", "Hello World");
+        hello.put("startParam",startParam);
+        
+
+        return hello;
     }
 }
